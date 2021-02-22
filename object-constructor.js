e5a31cca1f50abd386696a6da2cdbc1d73e84b3c
Andy Mopad <andy.mopad@gmail.com>
2021-02-22 12:46:59 +0000

    complete setup

--- object-constructor.js
+++ object-constructor.js
@@ -1,9 +1,28 @@
+
+
 function Book(title, author, pages, read) {
     this.name = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function () {
-        return (title + ' is a book by ' + author + ', ' + pages + ' pages, ' + read);
-    }
 }
+
+
+Book.prototype.info = function() {
+    return(this.name + ' is a book by ' + this.author + ', ' + this.pages + ' pages, ' + this.read);
+}
+//==============================================================================
+
+function Book(title, author, pages, read) {
+    this.name = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function() {
+        return(this.name + ' is a book by ' + this.author + ', ' + this.pages + ' pages, ' + this.read);}
+}
+  
+  const lolita = new Book('Lolita', 'Nabakov',333,'read')
+  const theremin = new Book('Theremin','Glinsky', 500, 'read')
+  console.log(lolita.info()) 
+  console.log(theremin.info()) 
