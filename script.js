5fe69af2a1caf631678423b848d62ff875ef1dbf
Andy Mopad <andy.mopad@gmail.com>
2021-02-23 11:28:21 +0000

    add delete book button

--- script.js
+++ script.js
@@ -22,17 +22,26 @@ addBookToLibrary('Lucky Jim','Kingsley Amis',293,'read')
 const library = document.getElementById('library');
 let bookContainer =[];
 let readButton=[];
+let removeButton=[]
 for (i = 0; i < myLibrary.length; i++) {
     bookContainer[i] = document.createElement('div');
     bookContainer[i].setAttribute('class', 'book');
     bookContainer[i].textContent = myLibrary[i].bookDisplay();
     library.appendChild(bookContainer[i]);
+    
     readButton[i] = document.createElement('button');
     readButton[i].setAttribute('class', 'read-button');
-    readButton[i].setAttribute('id', `${i}`);
+    readButton[i].setAttribute('id', `read-${i}`);
     readButton[i].textContent = myLibrary[i].read;
     bookContainer[i].appendChild(readButton[i]);
     readButton[i].setAttribute('onclick','changeRead('+`${i}`+')');
+    
+    removeButton[i] = document.createElement('button');
+    removeButton[i].setAttribute('class', 'remove-button');
+    removeButton[i].setAttribute('id', `remove-${i}`);
+    removeButton[i].textContent = 'delete';
+    bookContainer[i].appendChild(removeButton[i]);
+    removeButton[i].setAttribute('onclick','removeBook('+`${i}`+')');
 }
 
 document.getElementById('form').onsubmit = function() { 
@@ -45,12 +54,20 @@ document.getElementById('form').onsubmit = function() {
     bookContainer[bookIndex] = document.createElement('div');
     bookContainer[bookIndex].setAttribute('class', 'book');
     bookContainer[bookIndex].textContent = myLibrary[myLibrary.length-1].bookDisplay();
+    
     readButton[bookIndex] = document.createElement('button');
     readButton[bookIndex].setAttribute('class', 'read-button');
-    readButton[bookIndex].setAttribute('id', `${bookIndex}`);
+    readButton[bookIndex].setAttribute('id', `read-${bookIndex}`);
     readButton[bookIndex].textContent = myLibrary[bookIndex].read;
     bookContainer[bookIndex].appendChild(readButton[bookIndex]);
     readButton[bookIndex].setAttribute('onclick','changeRead('+`${bookIndex}`+')');
+    
+    removeButton[bookIndex] = document.createElement('button');
+    removeButton[bookIndex].setAttribute('class', 'remove-button');
+    removeButton[bookIndex].setAttribute('id', `remove-${bookIndex}`);
+    removeButton[bookIndex].textContent = 'delete';
+    bookContainer[bookIndex].appendChild(removeButton[bookIndex]);
+    removeButton[bookIndex].setAttribute('onclick','removeBook('+`${bookIndex}`+')');
     library.appendChild(bookContainer[bookIndex]);
     document.getElementById('title').value = ''
     document.getElementById('author').value = ''
@@ -68,7 +85,7 @@ function closeForm() {
   } 
 
 function changeRead(i) {
-    let changeButton = document.getElementById(i);
+    let changeButton = document.getElementById(`read-${i}`);
     console.log(changeButton)
     if(myLibrary[i].read === 'read') {
         changeButton.textContent = 'unread'
@@ -77,4 +94,7 @@ function changeRead(i) {
         changeButton.textContent = 'read'
         myLibrary[i].read = 'read'
     }
+}
+function removeBook(i) {
+    library.removeChild(bookContainer[i]);
 }
