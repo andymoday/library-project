a22d7147394daa5fa78528d515a448ad035f494f
Andy Mopad <andy.mopad@gmail.com>
2021-02-23 17:18:53 +0000

    complete majority of CSS work, begn implementation of local storage

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-let myLibrary = []
-
 function Book(title, author, pages, read) {
     this.name = title;
     this.author = author;
@@ -15,13 +13,19 @@ Book.prototype.bookDisplay = function() {
     return (`${this.name}\r\n${this.author}\r\n${this.pages} pages`);
 }
 
-addBookToLibrary('Lolita','Nabakov',123,'read')
-addBookToLibrary('LOTR','Tolkein',1234,'unread')
-addBookToLibrary('Lucky Jim','Kingsley Amis',293,'read')
-addBookToLibrary('Lolita','Nabakov',123,'read')
-addBookToLibrary('LOTR','Tolkein',1234,'unread')
-addBookToLibrary('Lucky Jim','Kingsley Amis',293,'read')
+//addBookToLibrary('Lolita','Nabakov',123,'READ')
+//addBookToLibrary('LOTR','Tolkein',1234,'UNREAD')
+//addBookToLibrary('Lucky Jim','Kingsley Amis',293,'READ')
+//addBookToLibrary('Lolita','Nabakov',123,'READ')
+//addBookToLibrary('LOTR','Tolkein',1234,'UNREAD')
+//addBookToLibrary('Lucky Jim','Kingsley Amis',293,'READ')
 
+let myLibrary = []
+console.log(localStorage.getItem('myLibrary'))
+if(localStorage.getItem('myLibrary')) {
+    myLibrary = JSON.parse(localStorage.getItem('myLibrary'));
+}
+console.log(Object.getPrototypeOf(myLibrary[1]))
 const library = document.getElementById('library');
 let bookContainer =[];
 let readButton=[];
@@ -42,42 +46,55 @@ for (i = 0; i < myLibrary.length; i++) {
     removeButton[i] = document.createElement('button');
     removeButton[i].setAttribute('class', 'remove-button');
     removeButton[i].setAttribute('id', `remove-${i}`);
-    removeButton[i].textContent = 'delete';
+    removeButton[i].textContent = 'DELETE';
     bookContainer[i].appendChild(removeButton[i]);
     removeButton[i].setAttribute('onclick','removeBook('+`${i}`+')');
 }
-
 document.getElementById('form').onsubmit = function() { 
     let title_input = document.getElementById('title').value;
     let author_input = document.getElementById('author').value;
-    let pages_input = document.getElementById('pages').value;
-    let read_input = (document.getElementById('read').checked === true) ? 'read' : 'unread';
-    addBookToLibrary(title_input,author_input,pages_input,read_input);
-    let bookIndex = myLibrary.length-1; 
-    bookContainer[bookIndex] = document.createElement('div');
-    bookContainer[bookIndex].setAttribute('class', 'book');
-    bookContainer[bookIndex].textContent = myLibrary[myLibrary.length-1].bookDisplay();
-    
-    readButton[bookIndex] = document.createElement('button');
-    readButton[bookIndex].setAttribute('class', 'read-button');
-    readButton[bookIndex].setAttribute('id', `read-${bookIndex}`);
-    readButton[bookIndex].textContent = myLibrary[bookIndex].read;
-    bookContainer[bookIndex].appendChild(readButton[bookIndex]);
-    readButton[bookIndex].setAttribute('onclick','changeRead('+`${bookIndex}`+')');
-    
-    removeButton[bookIndex] = document.createElement('button');
-    removeButton[bookIndex].setAttribute('class', 'remove-button');
-    removeButton[bookIndex].setAttribute('id', `remove-${bookIndex}`);
-    removeButton[bookIndex].textContent = 'delete';
-    bookContainer[bookIndex].appendChild(removeButton[bookIndex]);
-    removeButton[bookIndex].setAttribute('onclick','removeBook('+`${bookIndex}`+')');
-    library.appendChild(bookContainer[bookIndex]);
-    document.getElementById('title').value = ''
-    document.getElementById('author').value = ''
-    document.getElementById('pages').value = ''
-    document.getElementById('read').value = ''
-    return false;//It's important to return false; to prevent default behaviour at the end of your submit handler, as otherwise the form will post and reload the page.
+    let pages_input = parseInt(document.getElementById('pages').value);
+    if (typeof pages_input !== 'number') {
+        alert('Please enter a number')
+    } else {
+        let read_input = (document.getElementById('read').checked === true) ? 'READ' : 'UNREAD';
+        addBookToLibrary(title_input,author_input,pages_input,read_input);
+        let bookIndex = myLibrary.length-1; 
+        bookContainer[bookIndex] = document.createElement('div');
+        bookContainer[bookIndex].setAttribute('class', 'book');
+        bookContainer[bookIndex].textContent = myLibrary[myLibrary.length-1].bookDisplay();
+        
+        readButton[bookIndex] = document.createElement('button');
+        readButton[bookIndex].setAttribute('class', 'read-button');
+        readButton[bookIndex].setAttribute('id', `read-${bookIndex}`);
+        readButton[bookIndex].textContent = myLibrary[bookIndex].read;
+        bookContainer[bookIndex].appendChild(readButton[bookIndex]);
+        readButton[bookIndex].setAttribute('onclick','changeRead('+`${bookIndex}`+')');
+        
+        removeButton[bookIndex] = document.createElement('button');
+        removeButton[bookIndex].setAttribute('class', 'remove-button');
+        removeButton[bookIndex].setAttribute('id', `remove-${bookIndex}`);
+        removeButton[bookIndex].textContent = 'DELETE';
+        bookContainer[bookIndex].appendChild(removeButton[bookIndex]);
+        removeButton[bookIndex].setAttribute('onclick','removeBook('+`${bookIndex}`+')');
+        library.appendChild(bookContainer[bookIndex]);
+        document.getElementById('title').value = ''
+        document.getElementById('author').value = ''
+        document.getElementById('pages').value = ''
+        document.getElementById('read').value = ''
+        populateStorage();
+        console.log(Object.getPrototypeOf(myLibrary[bookIndex]))
+        return false;//It's important to return false; to prevent default behaviour at the end of your submit handler, as otherwise the form will post and reload the page.
+    }
 };
+function populateStorage() {
+        localStorage.setItem('myLibrary',JSON.stringify(myLibrary));
+}
+
+
+
+
+
 
 function openForm() {
     document.getElementById("myForm").style.display = "block";
@@ -90,14 +107,15 @@ function closeForm() {
 function changeRead(i) {
     let changeButton = document.getElementById(`read-${i}`);
     console.log(changeButton)
-    if(myLibrary[i].read === 'read') {
-        changeButton.textContent = 'unread'
-        myLibrary[i].read = 'unread'
+    if(myLibrary[i].read === 'READ') {
+        changeButton.textContent = 'UNREAD'
+        myLibrary[i].read = 'UNREAD'
     } else {
-        changeButton.textContent = 'read'
-        myLibrary[i].read = 'read'
+        changeButton.textContent = 'READ'
+        myLibrary[i].read = 'READ'
     }
 }
 function removeBook(i) {
     library.removeChild(bookContainer[i]);
+    delete myLibrary[i];
 }
